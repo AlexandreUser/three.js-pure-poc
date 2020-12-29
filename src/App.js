b5eed2f7d995ebbebb4a2261809d34c7e262c9b2
AlexandreUser <av.souza2018@gmail.com>
2020-12-29 15:41:20 -0300

    trying again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Renderer from "./components/rendererComponent"
 import Header from "./components/header"
-import pageStyle from "./styles/mainPage.module.css"
 import "./App.css"
 
 function App() {
